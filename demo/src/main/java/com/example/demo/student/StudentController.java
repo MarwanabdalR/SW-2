10693312d5ecf0d810120147427c34a95d578ab3
marwanMagdy66 <105191952+marwanMagdy66@users.noreply.github.com>
2024-05-04 06:40:09 +0300

    sdfsd

---
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+@RestController
+@RequestMapping(path="api/v1/student")
+
+public class StudentController {
+    private final StudentService StudentService;
+
+    public 
+@GetMapping("")
+
+
+}
